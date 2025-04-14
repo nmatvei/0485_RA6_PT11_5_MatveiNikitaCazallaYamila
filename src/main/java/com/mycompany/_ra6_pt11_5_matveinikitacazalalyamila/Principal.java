479e7272b72b43c16692196a1be1ab09ec522f83
nikita <nikitamatpech@gmail.com>
2025-04-14 13:57:31 +0200

    Versió incial del programa

---
+++ src/main/java/com/mycompany/_ra6_pt11_5_matveinikitacazalalyamila/Principal.java
@@ -0,0 +1,12 @@
+package com.mycompany._ra6_pt11_5_matveinikitacazalalyamila;
+
+/**
+ *
+ * @author Nikita i Yamila
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
