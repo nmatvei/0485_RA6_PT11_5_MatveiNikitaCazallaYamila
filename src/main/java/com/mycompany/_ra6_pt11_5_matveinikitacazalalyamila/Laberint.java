b8fa31630a7d296039dde42dc19f184cf6ba81c4
ycaza <ycaza@DESKTOP-T1UA6VD>
2025-04-17 17:10:23 +0200

    Afegiment de mètodes (es troben inacabats)

--- src/main/java/com/mycompany/_ra6_pt11_5_matveinikitacazalalyamila/Laberint.java
+++ src/main/java/com/mycompany/_ra6_pt11_5_matveinikitacazalalyamila/Laberint.java
@@ -1,54 +1,97 @@
 package com.mycompany._ra6_pt11_5_matveinikitacazalalyamila;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Nikita i Yamila
  */
 public class Laberint {
-    
+
     /*Declaració de variables i atributs*/
     private int altura;
     private int amplada;
     private int taulaMultiplicacio;
+    ArrayList<Integer> taulaMulti;
     private int[][] laberint;
     private final static int ALTURA_DEFAULT = 5;
     private final static int AMPLADA_DEFAULT = 8;
     private final static int TAULA_MULTIPLICACIO_DEFAULT = 5;
-    
+
     /**
      * Constructor per defecte que truca al parametritzat
      */
-    public Laberint(){
+    public Laberint() {
         this(ALTURA_DEFAULT, AMPLADA_DEFAULT, TAULA_MULTIPLICACIO_DEFAULT);
     }
-    
+
     /**
      * Constructor parametritzat per donar la dimensió desitjada al laberint i
      * determinar de quina taula de multiplicar es farà.
-     * 
+     *
      * @param altura
      * @param amplada
-     * @param taulaMultiplicacio 
+     * @param taulaMultiplicacio
      */
     public Laberint(int altura, int amplada, int taulaMultiplicacio) {
         this.altura = altura;
         this.amplada = amplada;
-        this.laberint = new int[amplada][altura];
+        this.laberint = new int[altura][amplada];
         this.taulaMultiplicacio = taulaMultiplicacio;
     }
-    
-    private void generarCami(){
-        
+
+    public void generarTaulaMult(int num) {
+
+        taulaMulti = new ArrayList<>();
+
+        for (int i = 0; i < num; i++) {
+            taulaMulti.add(i + 1);
+            System.out.println(i + 1);
+        }
+
     }
-    
-    private void omplirLaberint(){
-        
+
+    public void generarCami(int fila, int columna) {
+
+        boolean camiGenerat = false;
+
+        if (fila > 0 && columna > 0) {
+
+            while (!camiGenerat) {
+
+                int numAleatoriF = (int) (Math.random() * (fila));
+                int numAleatoriC = (int) (Math.random() * (columna));
+                int i = 0, j = 0;
+
+            }
+
+        } else if (fila > 0) {
+
+            for (int i = 0; i < fila; i++) {
+
+            }
+
+        } else if (columna > 0) {
+
+        }
+
     }
-    
+
+    public void omplirLaberint() {
+
+        for (int i = 0; i < altura; i++) {
+            for (int j = 0; j < amplada; j++) {
+                if (!(i == 0 && j == 0)) {
+                    laberint[i][j] = (int) (Math.random() * 101);
+                }
+            }
+        }
+    }
+
     /**
      * Mètode mostrarLaberint per mostrar el laberint amb un format de caselles
      */
-    public void mostrarLaberint(){
+    public void mostrarLaberint() {
         for (int i = 0; i < laberint.length; i++) {
             System.out.print("|");
             for (int j = 0; j < laberint.length; j++) {
--- src/main/java/com/mycompany/_ra6_pt11_5_matveinikitacazalalyamila/Principal.java
+++ src/main/java/com/mycompany/_ra6_pt11_5_matveinikitacazalalyamila/Principal.java
@@ -3,9 +3,9 @@ package com.mycompany._ra6_pt11_5_matveinikitacazalalyamila;
 import java.util.Scanner;
 
 /**
- * Script on es prova la clàsse Laberint, que ens retorna un petit laberint 
+ * Script on es prova la clàsse Laberint, que ens retorna un petit laberint
  * matemàtic de la taula que escollim.
- * 
+ *
  * @author Nikita i Yamila
  */
 public class Principal {
@@ -15,12 +15,56 @@ public class Principal {
         Scanner sc = new Scanner(System.in);
         Laberint laberint;
         boolean demanarDades = false;
-        
+        int taulaMult, files, columnes;
+
         System.out.println("DEMOSTRACIÓ DE LA CLASSE LABERINT");
-        while(!demanarDades){
-            try{
-                
-            } catch (Exception e) {System.err.println(e.getMessage());}
-        }
+
+        System.out.print("Taula de multiplicar (per defecte 5): ");
+        taulaMult = sc.nextInt();
+        
+        /*
+        boolean dimensionsCorrectes = false;
+
+        do {
+
+            int suma;
+
+            System.out.print("Dimensions de la matriu (per defecte 8x5):"
+                    + "\n\t- Files: ");
+            files = sc.nextInt();
+            System.out.print("\n\t- Columnes: ");
+            columnes = sc.nextInt();
+
+            suma = files + columnes;
+
+            if ((files == 12) || (columnes == 12)) {
+
+                if (suma == 12) {
+                    dimensionsCorrectes = true;
+                } else {
+                    System.out.println("Dimensions incorrectes.");
+                }
+
+            } else {
+
+                if (suma == 13) {
+                    dimensionsCorrectes = true;
+                } else {
+                    System.out.println("Dimensions incorrectes.");
+                }
+
+            }
+        } while (!dimensionsCorrectes);
+        */
+        System.out.println("5");
+        laberint = new Laberint();
+        System.out.println("gyh");
+        laberint.generarTaulaMult(taulaMult);
+        System.out.println("rtf");
+        laberint.omplirLaberint();
+        laberint.mostrarLaberint();
+        
+
     }
+
 }
